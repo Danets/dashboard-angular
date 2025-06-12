a1877d9b7ce515c4e1906fb2f6695a9e984466e1
Valeriy Danets <dantesvv@gmail.com>
2025-06-12 18:25:26 +0300

    Adding the basic navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Account } from './pages/account/account';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'account', component: Account },
+  { path: '**', component: Dashboard },
+];
