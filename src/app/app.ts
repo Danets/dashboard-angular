285528292a80ec1ee6f141522f46d893357cd93c
Valeriy Danets <dantesvv@gmail.com>
2025-06-14 17:30:52 +0300

    Were developed Header and Navigation components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { Navigation } from './shared/components/layout/navigation/navigation';
+import { Header } from './shared/components/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, Navigation, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
