020cf570cb51ecab984543086a4a18cb8180c8d0
Valeriy Danets <dantesvv@gmail.com>
2025-06-17 12:04:31 +0300

    Adding WorkQueue and Accounts components

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { WorkQueue } from './work-queue/work-queue';
+import { Accounts } from './accounts/accounts';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [WorkQueue, Accounts],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
