b281ca62faddf557778e4c5412573c28699c0c39
Valeriy Danets <dantesvv@gmail.com>
2025-06-18 15:14:39 +0300

    Were added PortfolioGoals, QuickActions and MarketIntel components into Dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { WorkQueue } from './work-queue/work-queue';
 import { Accounts } from './accounts/accounts';
+import { PortfolioGoals } from './portfolio-goals/portfolio-goals';
+import { QuickActions } from './quick-actions/quick-actions';
+import { MarketIntel } from './market-intel/market-intel';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [WorkQueue, Accounts],
+  imports: [WorkQueue, Accounts, PortfolioGoals, QuickActions, MarketIntel],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
---
+++ src/app/pages/dashboard/market-intel/market-intel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-intel',
+  imports: [],
+  templateUrl: './market-intel.html',
+  styleUrl: './market-intel.scss'
+})
+export class MarketIntel {
+
+}
