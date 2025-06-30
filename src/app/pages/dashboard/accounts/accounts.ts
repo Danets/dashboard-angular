c2e7a870cb8ac318c94c62803e57f89607c61f38
Valeriy Danets <dantesvv@gmail.com>
2025-06-30 17:51:47 +0300

    Has been rewriting styles for Accounts component

--- src/app/pages/dashboard/accounts/accounts.ts
+++ src/app/pages/dashboard/accounts/accounts.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, HostListener, input } from '@angular/core';
 import { IAccounts } from '../../../core/models/models';
 
 @Component({
@@ -9,4 +9,36 @@ import { IAccounts } from '../../../core/models/models';
 })
 export class Accounts {
   readonly data = input.required<IAccounts[]>();
+  menuId: string | null = null;
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.cell-actions')) {
+      this.menuId = null;
+    }
+  }
+
+  toggleMenu(event: MouseEvent, id: string) {
+    // event.stopPropagation();
+    this.menuId = this.menuId === id ? null : id;
+  }
+
+  getClassNameForChip(type: string) {
+    const convertedType = Number(type.slice(0, -1));
+    if (convertedType < 35) {
+      return 'green';
+    } else if (convertedType > 35 && convertedType < 60) {
+      return 'yellow';
+    } else {
+      return 'red';
+    }
+  }
+  getClassNameForStatus(status: string) {
+    const classList = {
+      active: status === 'Active',
+      review: status === 'Under review',
+    };
+    return classList;
+  }
 }
