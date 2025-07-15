4224319a89be8136fbcfea2404bc7731f6700fc3
Valeriy Danets <dantesvv@gmail.com>
2025-07-15 12:50:33 +0300

    Have been fixed styles in AccountHeader component

--- src/app/pages/account/account-header/account-header.ts
+++ src/app/pages/account/account-header/account-header.ts
@@ -7,40 +7,44 @@ import { Header } from '../../../core/models/models';
   template: `
     @if (header()) {
     <div class="account-header">
-      <div class="account-header__leftblock leftblock">
-        <div class="leftblock__avatar"></div>
-        <div class="leftblock__info info">
+      <div class="account-header__leftblock">
+        <div class="account-header__avatar"></div>
+        <div class="account-header__info info">
           <h2 class="info__title">{{ header().company }}</h2>
           <p class="info__adress">{{ header().address }}</p>
           <div class="info__person-block person-block">
-            <div class="person-block__item item">
-              <h5 class="item__title">Existing Account:</h5>
-              <div class="item__subtitle">
+            <div class="person-block__item">
+              <h5 class="person-block__title">Existing Account:</h5>
+              <div class="person-block__subtitle">
                 {{ header().existingAccount ? 'Yes' : 'No' }}
               </div>
             </div>
-            <div class="person-block__item item">
-              <h5 class="item__title">Submissions:</h5>
-              <div class="item__subtitle">{{ header().submissions }}</div>
+            <div class="person-block__item">
+              <h5 class="person-block__title">Submissions:</h5>
+              <div class="person-block__subtitle">
+                {{ header().submissions }}
+              </div>
             </div>
-            <div class="person-block__item item">
-              <h5 class="item__title">Broker:</h5>
-              <div class="item__subtitle">{{ header().broker }}</div>
+            <div class="person-block__item">
+              <h5 class="person-block__title">Broker:</h5>
+              <div class="person-block__subtitle">{{ header().broker }}</div>
             </div>
-            <div class="person-block__item item">
-              <h5 class="item__title">Underwriter:</h5>
-              <div class="item__subtitle">{{ header().underwriter }}</div>
+            <div class="person-block__item">
+              <h5 class="person-block__title">Underwriter:</h5>
+              <div class="person-block__subtitle">
+                {{ header().underwriter }}
+              </div>
             </div>
           </div>
         </div>
       </div>
       @if (header().needsAttention.length) {
-      <div class="account-header__rightblock rightblock">
-        <div class="rightblock__attention-box attention-box">
+      <div class="account-header__rightblock">
+        <div class="account-header__attention-box attention-box">
           <h4 class="attention-box__title">⚠️ Needs Attention</h4>
-          <ul class="attention-box__list list-box">
+          <ul class="attention-box__list">
             @for (item of header().needsAttention; track item) {
-            <li class="list-box__item">{{ item }}</li>
+            <li class="attention-box__item">{{ item }}</li>
             }
           </ul>
         </div>
