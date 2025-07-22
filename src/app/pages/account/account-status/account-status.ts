22d6f498313fae448213c5f7b34d4da21093f7d1
Valeriy Danets <dantesvv@gmail.com>
2025-07-22 12:40:09 +0300

    Have been implemented AccountStatus component

---
+++ src/app/pages/account/account-status/account-status.ts
@@ -0,0 +1,41 @@
+import { Account } from './../account';
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-account-status',
+  styleUrl: './account-status.scss',
+  template: `
+    <section class="account-status">
+      <!-- Status Steps -->
+      <div class="account-status__section">
+        <h2 class="account-status__title">Account Status</h2>
+
+        <ul class="account-status__steps">
+          @for (step of status(); track step) {
+          <li class="account-status__step">
+            <span class="account-status__label">{{ step }}</span>
+          </li>
+          }
+        </ul>
+      </div>
+
+      <!-- Compliance -->
+      <div class="account-status__section">
+        <div class="account-status__headline">
+          <h2 class="account-status__title">Compliance & Documentation</h2>
+          <a class="account-status__link">See history →</a>
+        </div>
+
+        <ul class="account-status__list">
+          @for (item of compliance(); track item) {
+          <li class="account-status__list-item">✅ {{ item }}</li>
+          }
+        </ul>
+      </div>
+    </section>
+  `,
+})
+export class AccountStatus {
+  readonly status = input.required<string[]>();
+  readonly compliance = input.required<string[]>();
+}
--- src/app/pages/account/account.ts
+++ src/app/pages/account/account.ts
@@ -3,10 +3,11 @@ import { AccountHeader } from './account-header/account-header';
 import { AccountDataService } from './account-data.service';
 import { PerformanceMetrics } from './performance-metrics/performance-metrics';
 import { Policies } from './policies/policies';
+import { AccountStatus } from './account-status/account-status';
 
 @Component({
   selector: 'app-account',
-  imports: [AccountHeader, PerformanceMetrics, Policies],
+  imports: [AccountHeader, PerformanceMetrics, Policies, AccountStatus],
   template: `
     <div class="account">
       <app-account-header [header]="accountData.header()!"></app-account-header>
@@ -14,6 +15,11 @@ import { Policies } from './policies/policies';
         [metrics]="accountData.performanceMetrics()!"
       ></app-performance-metrics>
       <app-policies [policies]="accountData.policies()!"></app-policies>
+      <app-account-status
+        [status]="accountData.status()!"
+        [compliance]="accountData.compliance()!"
+      >
+      </app-account-status>
     </div>
   `,
   styleUrl: './account.scss',
