8a91c0ccb63f0a34b03ea6ad227f09cf3eea95b2
Valeriy Danets <dantesvv@gmail.com>
2025-07-31 14:02:42 +0300

    Was added the expanded search bar into Header component

--- src/app/shared/components/layout/header/header.ts
+++ src/app/shared/components/layout/header/header.ts
@@ -3,9 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [],
-  templateUrl: './header.html',
-  styleUrl: './header.scss'
+  template: `
+    <header class="header">
+      <h1 class="header__headline">
+        Hi Arthur, welcome! You have 12 open tasks.
+      </h1>
+      <div class="header__search">
+        <input type="text" placeholder="Search" class="header__input" />
+        <button class="header__submit">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
+      <div class="header__user">AR</div>
+    </header>
+  `,
+  styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
